b25aaa334c8f122cdc8f33698b59199eb5023341
MariaSyrokvash <mariya.syrokvash@gmail.com>
2020-02-27 02:34:51 +0300

    feat: add solution reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
-}
+    var abs = Math.abs(n);
+
+    if (n < 0) {
+    return (abs.toString().split('').reverse().join(''));
+    } else {
+    return n.toString().split('').reverse().join('');
+  };
+};
